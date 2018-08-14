0fdd9abdcf713d1b7511c4d4b60a44251c2646ce
cgk3008 <36110991+cgk3008@users.noreply.github.com>
2018-08-14 11:02:46 -0400

    Added class Hacker exercise

--- JavaScript Exercises in CSharp/HackerRankExercises.cs
+++ JavaScript Exercises in CSharp/HackerRankExercises.cs
@@ -72,7 +72,7 @@ namespace JavaScript_Exercises_in_CSharp
             //    textWriter.Close();
             //}
 
-
+            // https://www.hackerrank.com/challenges/a-very-big-sum/problem
             //// Complete the aVeryBigSum function below.
             //static long aVeryBigSum(long[] ar)
             //{
@@ -179,54 +179,141 @@ namespace JavaScript_Exercises_in_CSharp
 
 
 
-         //   https://www.hackerrank.com/challenges/30-conditional-statements/problem?h_r=email&unlock_token=2af73adfd8abde4d2641b1b624435338fb7ba546&utm_campaign=30_days_of_code_continuous&utm_medium=email&utm_source=daily_reminder
-            static void Main(string[] args)
+            //        //   https://www.hackerrank.com/challenges/30-conditional-statements/problem?h_r=email&unlock_token=2af73adfd8abde4d2641b1b624435338fb7ba546&utm_campaign=30_days_of_code_continuous&utm_medium=email&utm_source=daily_reminder
+            //           static void Main(string[] args)
+            //           {
+            //Console.Write("Enter a number from 1 to 100 : ");
+
+            //               int N = Convert.ToInt32(Console.ReadLine());
+
+            //               bool even1 = N % 2 == 0;
+
+            //               if ( even1 == false)
+            //               {
+            //                   Console.WriteLine("Weird");
+            //               }
+            //               else if (even1 == true && N >= 2 && N <= 5)
+            //               {
+            //                   Console.WriteLine("Not Weird");
+            //               }
+
+            //               else if(even1 == true && N >= 6 && N <= 20)
+            //               {
+            //                   Console.WriteLine("Weird");
+            //               }
+            //               else if (even1 == true && N > 20)
+            //               {
+            //                   Console.WriteLine("Not Weird");
+            //               }
+
+
+
+            //               Console.ReadKey();
+
+            //           }
+
+            //    //https://www.hackerrank.com/challenges/plus-minus/problem
+            //    static void plusMinus(int[] arr)
+            //    {
+
+            //        double pos = 0;
+            //        double neg = 0;
+            //        double z = 0;
+            //        var res = new Array[1];
+
+            //        for (int i = 0; i < arr.Length;  i++)
+            //            if (arr[i] > 0)
+            //            {
+            //                pos += 1;
+
+            //            }
+            //        else if(arr[i] < 0)
+            //        {
+            //            neg += 1;
+
+            //        }
+            //        else if(arr[i] == 0)
+            //        {
+            //            z += 1;
+
+            //        }
+
+            //        Console.WriteLine(pos / arr.Length);
+            //        Console.WriteLine(neg / arr.Length);
+            //        Console.WriteLine(z / arr.Length);
+            //    }
+
+            //    static void Main(string[] args)
+            //    {
+            //        int n = Convert.ToInt32(Console.ReadLine());
+
+            //        int[] arr = Array.ConvertAll(Console.ReadLine().Split(' '), arrTemp => Convert.ToInt32(arrTemp))
+            //        ;
+            //        plusMinus(arr);
+            //    }
+
+            //}
+
+
+            //https://www.hackerrank.com/challenges/30-class-vs-instance/problem
+            class Person
             {
- Console.Write("Enter a number from 1 to 100 : ");
-
-                int N = Convert.ToInt32(Console.ReadLine());
-
-                bool even1 = N % 2 == 0;
-
-                if ( even1 == false)
+                public int age;
+                public Person(int initialAge)
                 {
-                    Console.WriteLine("Weird");
+                    // Add some more code to run some checks on initialAge
+                   if (initialAge > 0)
+                    {
+                        age = initialAge;
+                    }
+                   else
+                    {
+                       age = 0;
+                        Console.WriteLine("Age is not valid, setting age to 0.");
+                    }
                 }
-                else if (even1 == true && N >= 2 && N <= 5)
+                public void amIOld()
                 {
-                    Console.WriteLine("Not Weird");
+                    // Do some computations in here and print out the correct statement to the console
+                    if (age < 13)
+                    {
+                        Console.WriteLine("You are young.");
+                    }
+                    else if (age>= 13 && age < 18)
+                    {
+                        Console.WriteLine("You are a teenager.");
+                    }
+                    else if ( age >= 18)
+                    {
+                        Console.WriteLine("You are old.");
+                    }
                 }
 
-                else if(even1 == true && N >= 6 && N <= 20)
+                public void yearPasses()
                 {
-                    Console.WriteLine("Weird");
+                    // Increment the age of the person in here
+                    age = age + 1;
                 }
-                else if (even1 == true && N > 20)
+
+                static void Main(String[] args)
                 {
-                    Console.WriteLine("Not Weird");
+                    int T = int.Parse(Console.In.ReadLine());
+                    for (int i = 0; i < T; i++)
+                    {
+                        int age = int.Parse(Console.In.ReadLine());
+                        Person p = new Person(age);
+                        p.amIOld();
+                        for (int j = 0; j < 3; j++)
+                        {
+                            p.yearPasses();
+                        }
+                        p.amIOld();
+                        Console.WriteLine();
+                    }
                 }
-
-
-
-                Console.ReadKey();
-
             }
 
-
-
-
         }
-
-
-
-
-
-
-
-
-
-
-
     }
 }
 
