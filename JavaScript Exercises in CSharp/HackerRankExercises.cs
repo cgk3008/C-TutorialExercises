f11dfafd5fa7d74723c69a18b54da1737e8445e5
cgk3008 <36110991+cgk3008@users.noreply.github.com>
2018-08-15 11:12:56 -0400

    Loop exercise.

--- JavaScript Exercises in CSharp/HackerRankExercises.cs
+++ JavaScript Exercises in CSharp/HackerRankExercises.cs
@@ -72,6 +72,131 @@ namespace JavaScript_Exercises_in_CSharp
             //    textWriter.Close();
             //}
 
+
+
+
+
+
+            //factorial by recursive function
+
+            //function findFactorial(value1)
+            //{
+            //    //var value1 = Number(document.getElementById("1Fact").value);
+            //    {
+            //        if (value1 === 0)
+            //        {
+            //            value1 = 1;
+
+            //            return value1 * findFactorial(value1 - 1);
+            //        }
+            //        document.getElementById("resultF").innerHTML = value1;
+            //    }
+
+
+            ////closure in javascript
+            //let obj = function () {
+            //    let i = 0;
+
+            //    return {
+            //        setI(k) {
+            //            i = k;
+            //        },
+            //        getI() {
+            //            return i;
+            //        }
+            //    }
+            //};
+
+            //let x = obj();
+            //x.setI(2);
+            //x.setI(4);
+            //console.log(x.getI());
+
+
+
+
+
+            //was it a car or a cat i saw
+
+            //https://medium.freecodecamp.org/how-to-reverse-a-string-in-javascript-in-3-different-ways-75e4763c68cb
+            //Reverse a String With Recursion
+            //function reverseString(str) {
+            //    if (str === "") // This is the terminal case that will end the recursion
+            //        return "";
+
+            //    else
+            //        return reverseString(str.substr(1)) + str.charAt(0);
+            //    /* 
+            //    First Part of the recursion method
+            //    You need to remember that you won’t have just one call, you’ll have several nested calls
+            //    Each call: str === "?"        	                  reverseString(str.subst(1))     + str.charAt(0)
+            //    1st call – reverseString("Hello")   will return   reverseString("ello")           + "h"
+            //    2nd call – reverseString("ello")    will return   reverseString("llo")            + "e"
+            //    3rd call – reverseString("llo")     will return   reverseString("lo")             + "l"
+            //    4th call – reverseString("lo")      will return   reverseString("o")              + "l"
+            //    5th call – reverseString("o")       will return   reverseString("")               + "o"
+            //    Second part of the recursion method
+            //    The method hits the if condition and the most highly nested call returns immediately
+            //    5th call will return reverseString("") + "o" = "o"
+            //    4th call will return reverseString("o") + "l" = "o" + "l"
+            //    3rd call will return reverseString("lo") + "l" = "o" + "l" + "l"
+            //    2nd call will return reverserString("llo") + "e" = "o" + "l" + "l" + "e"
+            //    1st call will return reverserString("ello") + "h" = "o" + "l" + "l" + "e" + "h" 
+            //    */
+            //}
+            //reverseString("hello");
+
+
+
+
+
+            // //Sum an array
+            //        using System;
+            //using System.Collections.Generic;
+            //using System.IO;
+            //using System.Linq;
+
+            //class Solution
+            //            {
+
+            //                /*
+            //                 * Complete the simpleArraySum function below.
+            //                 */
+            //                static int simpleArraySum(int[] ar)
+            //                {
+            //                    int sum = 0;
+            //                    int i = 0;
+            //                    int n = ar.Length;
+            //                    for (i = 0; i < n; i++)
+            //                    {
+            //                        sum += ar[i];
+            //                    }
+            //                    return sum;
+            //                }
+
+            //                static void Main(string[] args)
+            //                {
+            //                    TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+            //                    int arCount = Convert.ToInt32(Console.ReadLine());
+
+            //                    int[] ar = Array.ConvertAll(Console.ReadLine().Split(' '), arTemp => Convert.ToInt32(arTemp))
+            //                    ;
+            //                    int result = simpleArraySum(ar);
+
+            //                    textWriter.WriteLine(result);
+
+            //                    textWriter.Flush();
+            //                    textWriter.Close();
+            //                }
+            //            }
+
+
+
+
+
+
+
             // https://www.hackerrank.com/challenges/a-very-big-sum/problem
             //// Complete the aVeryBigSum function below.
             //static long aVeryBigSum(long[] ar)
@@ -255,62 +380,83 @@ namespace JavaScript_Exercises_in_CSharp
             //}
 
 
-            //https://www.hackerrank.com/challenges/30-class-vs-instance/problem
-            class Person
+            ////https://www.hackerrank.com/challenges/30-class-vs-instance/problem
+            //class Person
+            //{
+            //    public int age;
+            //    public Person(int initialAge)
+            //    {
+            //        // Add some more code to run some checks on initialAge
+            //       if (initialAge > 0)
+            //        {
+            //            age = initialAge;
+            //        }
+            //       else
+            //        {
+            //           age = 0;
+            //            Console.WriteLine("Age is not valid, setting age to 0.");
+            //        }
+            //    }
+            //    public void amIOld()
+            //    {
+            //        // Do some computations in here and print out the correct statement to the console
+            //        if (age < 13)
+            //        {
+            //            Console.WriteLine("You are young.");
+            //        }
+            //        else if (age>= 13 && age < 18)
+            //        {
+            //            Console.WriteLine("You are a teenager.");
+            //        }
+            //        else if ( age >= 18)
+            //        {
+            //            Console.WriteLine("You are old.");
+            //        }
+            //    }
+
+            //    public void yearPasses()
+            //    {
+            //        // Increment the age of the person in here
+            //        age = age + 1;
+            //    }
+
+            //    static void Main(String[] args)
+            //    {
+            //        int T = int.Parse(Console.In.ReadLine());
+            //        for (int i = 0; i < T; i++)
+            //        {
+            //            int age = int.Parse(Console.In.ReadLine());
+            //            Person p = new Person(age);
+            //            p.amIOld();
+            //            for (int j = 0; j < 3; j++)
+            //            {
+            //                p.yearPasses();
+            //            }
+            //            p.amIOld();
+            //            Console.WriteLine();
+            //        }
+            //    }
+            //}
+
+
+
+
+
+
+            // https://www.hackerrank.com/challenges/30-loops/problem?h_r=email&unlock_token=575cdf50dea76cf06a564a4943c523eff4f088c8&utm_campaign=30_days_of_code_continuous&utm_medium=email&utm_source=daily_reminder
+
+
+            static void Main(string[] args)
             {
-                public int age;
-                public Person(int initialAge)
+                int n = Convert.ToInt32(Console.ReadLine());
+
+                var i = 1;
+                for (i = 1; i <= 10; i++)
                 {
-                    // Add some more code to run some checks on initialAge
-                   if (initialAge > 0)
-                    {
-                        age = initialAge;
-                    }
-                   else
-                    {
-                       age = 0;
-                        Console.WriteLine("Age is not valid, setting age to 0.");
-                    }
-                }
-                public void amIOld()
-                {
-                    // Do some computations in here and print out the correct statement to the console
-                    if (age < 13)
-                    {
-                        Console.WriteLine("You are young.");
-                    }
-                    else if (age>= 13 && age < 18)
-                    {
-                        Console.WriteLine("You are a teenager.");
-                    }
-                    else if ( age >= 18)
-                    {
-                        Console.WriteLine("You are old.");
-                    }
+                    Console.WriteLine("{0} x {1} = {2}", n, i, (n*i));
+
                 }
 
-                public void yearPasses()
-                {
-                    // Increment the age of the person in here
-                    age = age + 1;
-                }
-
-                static void Main(String[] args)
-                {
-                    int T = int.Parse(Console.In.ReadLine());
-                    for (int i = 0; i < T; i++)
-                    {
-                        int age = int.Parse(Console.In.ReadLine());
-                        Person p = new Person(age);
-                        p.amIOld();
-                        for (int j = 0; j < 3; j++)
-                        {
-                            p.yearPasses();
-                        }
-                        p.amIOld();
-                        Console.WriteLine();
-                    }
-                }
             }
 
         }
